41949f91e7ff8ceb0a8cf5fb25c7ac3e5db18380
alberto <albertobarbosa0003@gmail.com>
2022-04-22 14:13:37 -0300

    Adicionar arquivos de projeto.

---
+++ Pessoa/PessoaComptela.cs
@@ -0,0 +1,14 @@
+﻿using PessoaApp21;
+
+
+Pessoa pessoa = new Pessoa();
+
+pessoa.GetNome = Console.ReadLine();
+pessoa.GetIdade = int.Parse(Console.ReadLine());
+pessoa.GetProfissao = Console.ReadLine();
+
+
+
+Console.WriteLine($"Nome : {pessoa.Nome()}");
+Console.WriteLine($"Idade : {pessoa.Idade()}");
+Console.WriteLine($"Emprego : {pessoa.Emprego()}");
---
+++ Pessoa/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PessoaApp21
+{
+    internal class Pessoa
+    {
+        public string GetNome { get; set; } = default!;
+        public int GetIdade { get; set; }
+        public string GetProfissao { get; set; } = default!;
+
+        public string Nome()
+        {
+            return GetNome;
+        }
+        public int Idade()
+        {
+            return GetIdade;
+        }
+        public string Emprego()
+        {
+            return GetProfissao;
+        }
+    }
+}
